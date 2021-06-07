dcfd6e8a9c03f05d9fe8cf9fa6a2c53724fb6b9a
Philippe Perret <philippe.perret@yahoo.fr>
2021-06-07 06:23:43 +0200

    Suppression messages chargement des modules

--- ajax/required.rb
+++ ajax/required.rb
@@ -11,8 +11,6 @@ end
 # ---------------------------
 # Contient quelque chose comme '/Users/moi/Sites/MonApplication'
 #
-log("Chargement des modules")
 Dir["#{APP_FOLDER}/ajax/required/**/*.rb"].each do |m|
-  log("Chargement module '#{m}'")
   require m
 end
